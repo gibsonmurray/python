5727b6648e1dc3c19377a0de10ca1e9488a7f540
gibsonmurray <gibmurrays@gmail.com>
2022-07-29 21:28:09 -0400

    continuing

--- OriolsBaseball.py
+++ OriolsBaseball.py
@@ -1,11 +1,13 @@
 from ast import Num
 
 
-print("Type 1 to enter a number or 2 to enter a name:\n")
+print("Type 1 to enter a number or 2 to enter a name: ")
 x = input()
 if x == 1:
-  print("Enter player number:\n")
+  print("\nEnter player number: ")
   x = input()
   if x is 10 or x is 16 or x is 36 or x is 19:
-    print("Which player wears number " + x + " on his jersey?\n")
+    print("\nWhich player wears number " + x + " on his jersey? ")
+    x = input()
+    
     
