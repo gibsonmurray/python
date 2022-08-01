bdc66de443ef8f12e6d24259b4c6af4a3034eaab
gibsonmurray <gibmurrays@gmail.com>
2022-08-01 13:14:40 -0400

    v  b

--- OriolsBaseball.py
+++ OriolsBaseball.py
@@ -1,12 +1,15 @@
-jones = 10
-mancini = 16
-joseph = 36
-davis = 19
+players = {
+  "Jones" : 10,
+  "Mancini" : 16,
+  "Joseph" : 36,
+  "Davis" : 19
+}
 print("Type 1 to enter a number or 2 to enter a name: ")
-x = input()
-if x == 1:
+n = input()
+if n is 1:
   print("\nEnter player number: ")
-  x = input()
-  if x is 10 or x is 16 or x is 36 or x is 19:
-    print("\nWhich player wears number " + x + " on his jersey? ")
-    x = input()
+  n = input()
+  if n is 10 or n is 16 or n is 36 or n is 19:
+    print("\nWhich player wears number " + n + " on his jersey? ")
+    name = input()
+    if name
