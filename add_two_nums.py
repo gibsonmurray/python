57115a8d97d5b82ada993315fd194804e71ee4f3
gibsonmurray <gibmurrays@gmail.com>
2022-08-04 12:48:27 -0400

    renamed hello_world and updated add_two_nums

--- add_two_nums.py
+++ add_two_nums.py
@@ -1,3 +1,7 @@
+from asyncio.windows_events import NULL
+from typing import List
+
+
 class ListNode:
   def __init__(self, val=0, next=None):
     self.val = val
@@ -6,6 +10,21 @@ class ListNode:
 def addTwoNumbers(l1, l2):
   num1 = 0
   num2 = 0
-
-  
-  while 
+  while l1 != None and l2 != None:
+    num1 += l1.val
+    if (l1.next != None):
+      num1 *= 10
+    l1 = l1.next
+    num2 += l2.val
+    if (l2.next != None):
+      num2 *= 10
+    l2 = l2.next
+  new_num = num1 + num2
+  if new_num == 0:
+    ans = ListNode()
+  else:
+    new_val = new_num % 10
+    ans = ListNode()
+    while new_num > 0:
+      
+      new_node = ListNode()
