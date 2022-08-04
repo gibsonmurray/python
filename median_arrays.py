77c1a8390ea0cc473f06c2e6850c52564e82686d
gibsonmurray <gibmurrays@gmail.com>
2022-08-04 11:33:30 -0400

    adding tests

--- median_arrays.py
+++ median_arrays.py
@@ -10,3 +10,8 @@ def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
   elif len(merged) == 1:
     ans = merged[0]
   return ans
+
+def tests():
+  nums1 = [1, 3]
+  nums2 = [2]
+  print(findMedianSortedArrays(nums1, nums2))
