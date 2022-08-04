4b428014d7fd168fd483e362d64cbf700ce8bfec
gibsonmurray <gibmurrays@gmail.com>
2022-08-04 11:31:52 -0400

    leetcode median_arrays

---
+++ median_arrays.py
@@ -0,0 +1,12 @@
+def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+  ans = 0
+  merged = nums1 + nums2
+  merged.sort()
+  while len(merged) is not 1 or 2:
+    merged.pop()
+    merged.pop(0)
+  if len(merged) == 2:
+    ans = (merged[0] + merged[1]) / 2
+  elif len(merged) == 1:
+    ans = merged[0]
+  return ans
