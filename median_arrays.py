6ee6b0383b17afa5ea70841df08689aa08d912ab
gibsonmurray <gibmurrays@gmail.com>
2022-08-04 11:45:09 -0400

    finished

--- median_arrays.py
+++ median_arrays.py
@@ -11,6 +11,6 @@ def findMedianSortedArrays(nums1, nums2) -> float:
     ans = merged[0]
   return ans
 
-nums1 = [1, 3]
-nums2 = [2]
+nums1 = [1, 2, 234, 656, 765, 4, 53, 5 ,5 ,35]
+nums2 = [3, 4, 34, 45456, 467, 890, 234]
 print(findMedianSortedArrays(nums1, nums2))
