9cd7f82806d6cbd3d2f330d5529ba359e2792bc1
gibsonmurray <gibmurrays@gmail.com>
2022-08-11 11:39:34 -0400

    updating

--- roman_to_int.py
+++ roman_to_int.py
@@ -2,11 +2,10 @@ def intToRoman(num):
   romans = {1000 : 'M',900 : 'CM',  500 : 'D', 400 : 'CD', 100 : 'C', 90 : 'XC', 
             50 : 'L', 40 : 'XL', 10 : 'X', 9 : 'IX', 5: 'V', 4 : 'IV', 1: 'I'}
   ans = ''
-  while num > 0:
-    for key in romans:
-      while num / key > 1:
-        ans = ans + romans.get(key)
-        num = num // key
+  for key in romans:
+    while num / key >= 1:
+      ans = ans + romans.get(key)
+      num = num - key
   return ans
 
 #Tests
