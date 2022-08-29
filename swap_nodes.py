91a75017aca16ef66757daf6ded48fc47ce614ac
gibsonmurray <gibmurrays@gmail.com>
2022-08-29 10:04:00 -0400

    updated

--- swap_nodes.py
+++ swap_nodes.py
@@ -1,27 +1,35 @@
 import linked_list
 
 def swapNodes(head: linked_list.ListNode, k: int) -> linked_list.ListNode:
-  # tracker = []
-  # curr = head
-  # while curr != None:
-  #   tracker.append(curr.val)
-  #   curr = curr.next
+  n = 0
   curr = head
-  # First & last node case
-  if k == 1:
+  while curr != None:
+    n = n + 1
+  curr = head # resets curr to head
+  # Case 1: odd number of nodes, swaps middle node : nothing changes
+  if n % 2 == 1:
+    # Do nothing
+    return head
+  # Case 2: if k = 1 or n
+  elif k == 1 or k == n:
     while curr.next != None:
       curr = curr.next
-    curr.next = head
-    curr = head
-    while curr.next.next != head:
-      curr = curr.next
-    temp = curr.next
-    curr.next = head
+    temp = curr #copies last node
+    curr = head 
     temp.next = head.next
-    head.next = temp
-    head.next = None
+    curr.next = None
     head = temp
-  return head
+  # Case 3: if k <= n/2
+  elif k <= n/2:
+    for i in range(1, k):
+      curr = curr.next
+    temp1 = curr
+    for i in range(1, n - (2 * k)):
+      curr = curr.next
+    temp2 = curr
+    # continue to swap
+  # Case 4: k >= n/2
+
     
     
 #Tests
