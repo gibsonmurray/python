d3143885ed2260e3c943fe4a43614c3885551770
gibsonmurray <gibmurrays@gmail.com>
2022-09-28 15:46:22 -0400

    updated

--- str_to_int.py
+++ str_to_int.py
@@ -1,10 +1,18 @@
+from operator import truediv
 import re
 
 def myAtoi(s: str) -> int:
     s = s.strip()
-    if s != str.empty:
-        sign = s[:1]
-        ans = re.search("^\d", s[1:])
-        ans = int(ans)
-        
-        
+    negative = False
+    if s[:1] == "-":
+        negative = True
+        ans = int(re.search("^\d", s[1:]).string)
+    else: 
+        ans = int(re.search("^\d", s).string)
+    if (-2)**31 > ans or ans > (2**31) - 1:
+        ans = 0
+    if negative:
+        ans *= (-1)
+    return ans
+
+myAtoi("4193 with words")        
